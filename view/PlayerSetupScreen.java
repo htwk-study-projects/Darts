aed1976bf3258b450647246b1e714bb7b3a7e3bb
krapottke <krapottke_git@icloud.com>
2024-08-23 16:14:52 +0200

    PlayerSetupScreen Hintergrund eingefärbt

--- view/PlayerSetupScreen.java
+++ view/PlayerSetupScreen.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -10,23 +11,28 @@ import javax.swing.SwingConstants;
 public class PlayerSetupScreen extends JPanel {
 
     private JLabel title;
+    private Bar titleBar;
+    
+    private Line startBreakLine;
+    private Bar startBreakBar;
     protected JButton startButton;
     protected JButton backButton;
     
-    PlayerNameInput playerNameInput;
+    private PlayerNameInput playerNameInput;
 
     
     public PlayerSetupScreen() {
+    	
+    	Color background = Color.LIGHT_GRAY;
 
         this.setLayout(new GridLayout(3, 3));
         DartsGUI.gridLayoutFill(this, 3, 3);
         title = new JLabel("Player Setup");
         title.setFont(DartsGUI.FONT_TITLE);
         title.setHorizontalAlignment(SwingConstants.CENTER);
-
-        // Panel für den Titel
         JComponent[] titleBarElements = {title, new TransparentPanel()};
-        Bar titleBar = new Bar(titleBarElements);
+        titleBar = new Bar(titleBarElements);
+        titleBar.setBackground(background);
         titleBar.barPlacement(this, 1);
 
         // Erstellen der Buttons
@@ -34,12 +40,15 @@ public class PlayerSetupScreen extends JPanel {
         backButton = new JButton("Zurück");
         JButton[] setupButtons = {backButton, startButton};
         DartsGUI.fontAdjust(DartsGUI.FONT_BIG, setupButtons);
-        Line startBreakLine = new Line(setupButtons);
+        startBreakLine = new Line(setupButtons);
+        startBreakLine.setOpaque(false);
         JComponent[] startBreakBarElements = {new TransparentPanel(), new TransparentPanel(), new TransparentPanel(), startBreakLine};
-        Bar startBreakBar = new Bar(startBreakBarElements);
+        startBreakBar = new Bar(startBreakBarElements);
+        startBreakBar.setBackground(background);
         startBreakBar.barPlacement(this, 7);;
         
        playerNameInput = new PlayerNameInput();
+       playerNameInput.setBackground(background);
        
        this.remove(4);
        this.add(playerNameInput, 4); //Spieler-Label und Combo Boxen
