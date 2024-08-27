16c8531957ec2451d6dd3a64fecbcc4d6039f07d
krapottke <krapottke_git@icloud.com>
2024-08-27 18:15:11 +0200

    Spielname wird im gamescreen angezeigt

--- controller/PlayerSetupController.java
+++ controller/PlayerSetupController.java
@@ -35,6 +35,15 @@ public class PlayerSetupController {
 	private void initPlayerSetup() {
 	    readPlayerSetup();
 	    writePlayerSetup();
+	    updateNextScreen();
+
+	    if (textFieldFullfillmentCondition() && colorPlayerFullfilmentCondition()) cardLayout.show(screenToControl.getRootPane().getContentPane(), "game");
+	    if (!textFieldFullfillmentCondition()) popUpNotificationTextField();
+	    if (!colorPlayerFullfilmentCondition()) popUpNotificationColorPlayer();
+	}
+
+	private void updateNextScreen() {
+		screenToAdjustDisplay.getGameScreenSideBar().setGameNameLabel(gameName);
 	    screenToAdjustDisplay.getGameScreenSideBar().setPlayerTableData(data.preparePlayerDataForTable());
 	    screenToAdjustDisplay.getGameScreenSideBar().getPlayerPanel().setLabelTexts(
 	        playerNames[0], 
@@ -43,12 +52,8 @@ public class PlayerSetupController {
 	        data.getPlayers()[0].getPlayerDarts()[0].getPoints().toString(),
 	        data.getPlayers()[0].getPlayerDarts()[0].getPoints().toString()
 	    );
-
-	    if (textFieldFullfillmentCondition() && colorPlayerFullfilmentCondition()) cardLayout.show(screenToControl.getRootPane().getContentPane(), "game");
-	    if (!textFieldFullfillmentCondition()) popUpNotificationTextField();
-	    if (!colorPlayerFullfilmentCondition()) popUpNotificationColorPlayer();
 	}
-
+	
 	
 	private void writePlayerSetup() {
 		this.data.setGameName(gameName);
