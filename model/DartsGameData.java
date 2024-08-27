106382b564c1aa2469f1b604e8515fa98c539995
henryxtn <henryxtn@icloud.com>
2024-08-27 10:50:44 +0200

    getcurrentPlayer angepasst

--- model/DartsGameData.java
+++ model/DartsGameData.java
@@ -3,72 +3,93 @@ package model;
 import java.awt.Color;
 
 public class DartsGameData {
-	
-	private static int gameIDCounter = 0;
-	
-	private int gameID;
-	private String gameName;
-	private int playerCount;
-	private Player[] players;
-	private GameMode gameMode;
-	
-	public DartsGameData(){
-		setGameIDCounter(10);
-		this.gameID = gameIDCounter;
-		gameIDCounter++;
-	}
-	
-	public static void setGameIDCounter(int gameIDCounter) {
-		DartsGameData.gameIDCounter = gameIDCounter;
-	}
+    
+    private static int gameIDCounter = 0;
+    
+    private int gameID;
+    private String gameName;
+    private int playerCount;
+    private Player[] players;
+    private GameMode gameMode;
+    private int currentPlayerIndex = 0; // Index des aktuellen Spielers
+    private int turnCount = 0; // Zähler für die Züge eines Spielers
 
-	public void setGameName(String gameName) {
-		this.gameName = gameName;
-	}
+    public DartsGameData(){
+        setGameIDCounter(10);
+        this.gameID = gameIDCounter;
+        gameIDCounter++;
+    }
+    
+    public static void setGameIDCounter(int gameIDCounter) {
+        DartsGameData.gameIDCounter = gameIDCounter;
+    }
 
-	public void setPlayers(String[] playerNames, Color[] playerColors, int gamePoints) {
-		this.players = Player.createPlayer(playerNames, playerColors, gamePoints);;
-	}
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
 
-	public void setGameMode(int gamePoints, String inMode, String outMode) {
-		this.gameMode = new GameMode(gamePoints, inMode, outMode);
-	}
+    public void setPlayers(String[] playerNames, Color[] playerColors, int gamePoints) {
+        this.players = Player.createPlayer(playerNames, playerColors, gamePoints);
+        this.playerCount = players.length; // Spieleranzahl setzen
+    }
 
-	public int getGameID() {
-		return gameID;
-	}
+    public void setGameMode(int gamePoints, String inMode, String outMode) {
+        this.gameMode = new GameMode(gamePoints, inMode, outMode);
+    }
 
-	public String getGameName() {
-		return gameName;
-	}
+    public int getGameID() {
+        return gameID;
+    }
 
-	public Player[] getPlayers() {
-		return players;
-	}
+    public String getGameName() {
+        return gameName;
+    }
 
-	public GameMode getGameMode() {
-		return gameMode;
-	}
+    public Player[] getPlayers() {
+        return players;
+    }
 
-	public int getPlayerCount() {
-		return playerCount;
-	}
+    public GameMode getGameMode() {
+        return gameMode;
+    }
 
-	public void setPlayerCount(int playerCount) {
-		this.playerCount = playerCount;
-	}
-	
-	public Player getCurrentPlayer() {
-		return players[0];
-	
-	}
-	
-	public Object[][] preparePlayerDataForTabel(){
-		Object[][] playerDataForTable = new Object[this.playerCount][3] ;
-		for(int i = 0; i < playerDataForTable.length; i++) {
-			playerDataForTable[i] = new Object[] {players[i].getColor(), players[i].getPlayerPoints(), players[i].getName()};
-		}
-		return playerDataForTable;
-	}
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    public void setPlayerCount(int playerCount) {
+        this.playerCount = playerCount;
+    }
+    
+
+    public Player getCurrentPlayer() {
+        while (true) {
+            Player currentPlayer = players[currentPlayerIndex];
+            
+          
+            if (currentPlayer.getStatusFinish() == true) {
+                currentPlayerIndex = (currentPlayerIndex + 1) % playerCount; 
+                continue; 
+            }
+            
+            turnCount++;
+            if (turnCount >= 3) { 
+                turnCount = 0;
+                currentPlayerIndex = (currentPlayerIndex + 1) % playerCount;
+            }
+            
+            return currentPlayer;
+        }
+    }
+    
+    public Object[][] preparePlayerDataForTable(){
+        Object[][] playerDataForTable = new Object[this.playerCount][3];
+        for(int i = 0; i < playerDataForTable.length; i++) {
+            playerDataForTable[i] = new Object[] {players[i].getColor(), players[i].getPlayerPoints(), players[i].getName()};
+        }
+        return playerDataForTable;
+    }
+    
 
 }
+
